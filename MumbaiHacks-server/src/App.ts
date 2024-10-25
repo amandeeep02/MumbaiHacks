75dbf23852edf8b1dca3505f0312f77a3b6641f1
amandeeep02 <amandeepsr02@gmail.com>
2024-10-26 02:41:50 +0530

    feat: Add telegram bot

--- MumbaiHacks-server/src/App.ts
+++ MumbaiHacks-server/src/App.ts
@@ -52,9 +52,6 @@ app.use(compression());
 app.use(passport.initialize());
 passportInit(passport);
 
-// Routes
-routes(app);
-
 // Global error handler
 app.use(
   globalErrorHandler as unknown as (
