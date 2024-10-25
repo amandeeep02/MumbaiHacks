40be818798b1069c3390201f76d345fed7c1b78f
Aditya Belgaonkar <adityabelgaonkar.30@gmail.com>
2024-10-25 22:23:20 +0530

    SHYSHTEM HANG

--- MumbaiHacks-client/src/router/routes.tsx
+++ MumbaiHacks-client/src/router/routes.tsx
@@ -1,10 +1,12 @@
 import { Landing } from '@/pages/Landing'
+import Chat from '@/pages/Chat'
 import { Route, Routes } from 'react-router-dom'
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="*" element={<Landing />} />
+      <Route path="/chat" element={<Chat />} />
     </Routes>
   )
 }
